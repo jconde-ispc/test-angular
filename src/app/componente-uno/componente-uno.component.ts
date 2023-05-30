79906703e50003067b0424c8fdec415140b37630
Julián <juliancond@gmail.com>
2023-05-30 11:19:59 -0300

    cambios para usos ngmodel

--- src/app/componente-uno/componente-uno.component.ts
+++ src/app/componente-uno/componente-uno.component.ts
@@ -12,11 +12,13 @@ export class ComponenteUnoComponent implements OnInit {
   avatar1:string = "https://www.w3schools.com/w3css/img_avatar3.png";
 
   avatar:string = "img/community.svg";
+  avatar2:string = "img/community.svg";
 
   person = {
     age : 19,
     name: "Antonia",
-    avatar:"https://www.w3schools.com/w3css/img_avatar.png"
+    avatar:"https://www.w3schools.com/w3css/img_avatar.png",
+    avatar2: "img/community.svg"
   }
 
 
